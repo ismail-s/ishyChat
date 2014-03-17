cda5ca525f64ec64308fe8752cda6e63a4a5fc0c
ismail-s <ismail-s@users.noreply.github.com>
2014-03-17 12:40:17 +0000

    Removed license header

--- ishyChat/Client/Networking.py
+++ ishyChat/Client/Networking.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#  untitled.py
-#
-#  Copyright 2013 pt-id.tk
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#  Also, this software should only be used for good and not evil.
-#
 """
 This is the main client file for isyChat. It contains code for making
 The application and setting up and managing the connection to the
