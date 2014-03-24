61ede97de14889154a0a0300197eaba9c257b1ea
ismail-s <ismail-s@users.noreply.github.com>
2014-03-24 16:52:43 +0000

    Add tests for Encryptor.py

--- tests.py
+++ tests.py
@@ -3,15 +3,14 @@ import unittest
 from ishyChat.Utils import Messages, Packer, Encryptor
 
 class TestPacker(unittest.TestCase):
-    def setUp(self):
-        self.dict_to_pack = {'test': 1,
-                             'test1': 2,
-                             'test2': 3}
 
     def test_packer_can_turn_some_dict_into_string_and_back(self):
-        res = Packer.packUp(self.dict_to_pack)
+        dict_to_pack = {'test': 1,
+                        'test1': 2,
+                        'test2': 3}
+        res = Packer.packUp(dict_to_pack)
         res = Packer.packDown(res)
-        self.assertEqual(res, self.dict_to_pack)
+        self.assertEqual(res, dict_to_pack)
 
     def test_packer_can_deal_with_packing_and_unpacking_empty_strings(self):
         res = Packer.packDown(Packer.packUp({}))
@@ -29,8 +28,34 @@ class TestPacker(unittest.TestCase):
         res = Packer.packDown(res)
         self.assertIsInstance(res, dict)
     
+class TestEncryptor(unittest.TestCase):
+    def setUp(self):
+        self.enc = Encryptor.Encryptor('some key')
+        self.test_str = 'some random text'
+        
+    def test_encrypt_and_decrypt_are_inverse_of_each_other(self):
+        res = self.enc.encrypt(self.test_str)
+        res = self.enc.decrypt(*res)
+        self.assertEqual(res, self.test_str)
     
-
+    def test_encrypt_and_decrypt_work_on_empty_strings(self):
+        res = self.enc.encrypt('')
+        res = self.enc.decrypt(*res)
+        self.assertEqual(res, '')
+    
+    def test_ECB_encryption_on_normal_string(self):
+        res = self.enc.encrypt_ECB(self.test_str)
+        res = self.enc.decrypt_ECB(res)
+        self.assertEqual(res, self.test_str)
+    
+    def test_ECB_encryption_on_empty_string(self):
+        res = self.enc.encrypt_ECB('')
+        res = self.enc.decrypt_ECB(res)
+        self.assertEqual(res, '')
+    
+    
+    
+    
 
 if __name__ == '__main__':
     unittest.main()
