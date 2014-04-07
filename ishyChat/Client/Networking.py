9026db4ec8721dc648d52b8e30a850bfdfe86709
Ismail Suleman <astroman3D@gmail.com>
2014-04-07 15:30:11 +0100

    Move duplicated code to separate class
    BaseNetworking now holds a class that is subclassed by the
    ClientConnection classes in Networking and Py3Networking.

--- ishyChat/Client/Networking.py
+++ ishyChat/Client/Networking.py
@@ -28,12 +28,14 @@ import ishyChat.Utils.Packer as Pk
 #import ishyChat.Utils.Encryptor as Encryptor
 #These are messages to display to the user
 import ishyChat.Utils.Messages as Messages
+
+from ishyChat.Client.BaseNetworking import BaseConnection
 ################
 ###End imports##
 ################
 
 
-class ClientConnection(LineReceiver):
+class ClientConnection(BaseConnection, LineReceiver):
     """This class handles sending and receiving messages
     
     to/from the server. It also deals with some metadata
@@ -42,96 +44,10 @@ class ClientConnection(LineReceiver):
     which don't depend on what sort of GUI or CLI interface
     you're using)."""
     def __init__(self, factory, application, *args, **kwargs):
-        self.factory = factory
-        self.app = application
-        #Set up the encryption-getting rid of this
-        #self.encryptor = Encryptor.Encryptor(key)
+        BaseConnection.__init__(self, factory, application, *args, **kwargs)
 
-    def connectionMade(self):
-        self.lineReceived(Messages.start_message)
-        self.factory.state = "CONNECTED"
-
-    def lineReceived(self, line):
-        if not line: return  # If we haven't been given anything, then we don't do anything.
-        dict_obj = Pk.packDown(line)
-        name, msg, metadata = dict_obj['name'], dict_obj['message'], dict_obj['metadata']
-        name_tag = ''
-        if 'server' == name:
-            if 'getname' in metadata:
-                self.factory.state = "GET NAME"
-            elif 'gotname' in metadata:
-                self.factory.state = "CONNECTED"
-                self.getUsers()
-                self.app.addClient(self.name)
-            elif 'pong' in metadata:
-                msg = 'ping time: ' + str(time.clock() - self.ping_start)
-            elif 'newclient' in metadata:
-                new_name = msg[:msg.find(' ')]
-                self.app.addClient(new_name)
-            elif 'lostclient' in metadata:
-                name_to_remove = msg[:msg.find(' ')]
-                self.app.removeClient(name_to_remove)
-            elif 'gotusers' in metadata:
-                list_of_users = metadata['gotusers']
-                self.app.addClients(*list_of_users)
-                msg = 'Users in chatroom: ' + ' '.join(list_of_users)
-        
-        elif 'client' != name:
-            name_tag = name
-        self.app.addString(msg, name_tag)
-
-    def sendLine(self, line):
-        """Sends line, but only after checking to see
-        
-        if anything special need to be done (this is
-        what self._command_parser does)."""
-        state = self.factory.state
-        if any((not line,
-               self._command_parser(line),
-               state == "NOT CONNECTED")): return
-        
-        if state == "GET NAME":
-            self.name = line
-            #self.name_enc = self.encryptor.encrypt_ECB(self.name)
-            dict_to_send = Pk.makeDict(name=self.name, metadata={'newname': None})
-        elif state == "CONNECTED":
-            #iv, message = self.encryptor.encrypt(line)
-            dict_to_send = Pk.makeDict(name=self.name, msg=line)
-        line = Pk.packUp(dict_to_send)
+    def write(self, line):
         LineReceiver.sendLine(self, line)
-
-    def _command_parser(self, line):
-        """Checks if there are any commands in line.
-        
-        If there are, then they are dealt with. Note that
-        GUI specific commands should have been dealt with
-        already.
-        
-        If this function return True, then that means that
-        the calling function should not print line.
-        If it returns False, then the calling function
-        *should* print line to the screen (ie insert it
-        into the entrybox)."""
-        if not line.startswith('/'): return
-        line = line[1:].lower()
-        if line == 'warning':
-            self.lineReceived(Messages.warning_message)
-            return True
-        elif line == 'ping':
-            if self.factory.state == "CONNECTED":
-                LineReceiver.sendLine(self, Messages.ping_message)
-                self.ping_start = time.clock()
-                return True
-        elif any((line == 'list', line == 'listusers')):
-            if self.factory.state == "CONNECTED":
-                self.getUsers()
-                return True 
-        return False
-
-    def getUsers(self):
-        """Asks the server for a list of people in the chatroom"""
-        LineReceiver.sendLine(self, Messages.getusers_message)
-
 
 class Factory(ReconnectingClientFactory):
     """Sets up a connection, repeatedly trying to remake
--- ishyChat/Client/Py3Networking.py
+++ ishyChat/Client/Py3Networking.py
@@ -12,7 +12,7 @@ Run this file to run ishyChat client.
 ########################
 #This is for timing pings.
 import time
-
+import ssl
 
 import asyncio
 ##The last 3 imports are local files.
@@ -22,12 +22,14 @@ import ishyChat.Utils.Packer as Pk
 #import ishyChat.Utils.Encryptor as Encryptor
 #These are messages to display to the user
 import ishyChat.Utils.Messages as Messages
+
+from ishyChat.Client.BaseNetworking import BaseConnection
 ################
 ###End imports##
 ################
 
 
-class ClientConnection(asyncio.Protocol):
+class ClientConnection(BaseConnection, asyncio.Protocol):
     """This class handles sending and receiving messages
     
     to/from the server. It also deals with some metadata
@@ -36,19 +38,13 @@ class ClientConnection(asyncio.Protocol):
     which don't depend on what sort of GUI or CLI interface
     you're using)."""
     def __init__(self, factory, application, *args, **kwargs):
-        # LineReceiver.__init__(self, *args, **kwargs)
-        self.factory = factory
-        self.app = application
-        #Set up the encryption-getting rid of this
-        #self.encryptor = Encryptor.Encryptor(key)
+        BaseConnection.__init__(self, factory, application, *args, **kwargs)
 
     def connection_made(self, transport):
-        self.data_received(Messages.start_message)
-        self.factory.state = "CONNECTED"
+        BaseConnection.connection_made(self)
         self.transport = transport
 
     def data_received(self, line):
-        if not line: return  # If we haven't been given anything, then we don't do anything.
         if isinstance(line, bytes):
             line = line.decode()
         # Note that line has '\r\n' at the end of it,
@@ -56,94 +52,14 @@ class ClientConnection(asyncio.Protocol):
         # with json decoding
         #while not line.endswith('}'):
             #line = line[:-1]
-        dict_obj = Pk.packDown(line)
-        name, msg, metadata = dict_obj['name'], dict_obj['message'], dict_obj['metadata']
-        name_tag = ''
-        if 'server' == name:
-            if 'getname' in metadata:
-                self.factory.state = "GET NAME"
-            elif 'gotname' in metadata:
-                self.factory.state = "CONNECTED"
-                self.getUsers()
-                self.app.addClient(self.name)
-            elif 'pong' in metadata:
-                msg = 'ping time: ' + str(time.clock() - self.ping_start)
-            elif 'newclient' in metadata:
-                new_name = msg[:msg.find(' ')]
-                self.app.addClient(new_name)
-            elif 'lostclient' in metadata:
-                name_to_remove = msg[:msg.find(' ')]
-                self.app.removeClient(name_to_remove)
-            elif 'gotusers' in metadata:
-                list_of_users = metadata['gotusers']
-                self.app.addClients(*list_of_users)
-                msg = 'Users in chatroom: ' + ' '.join(list_of_users)
-        
-        elif 'client' != name:
-            name_tag = name
-        self.app.addString(msg, name_tag)
+        BaseConnection.data_received(self, line)
 
-    def sendLine(self, line):
-        return self.send_data(line)
-
-    def send_data(self, line):
-        """Sends line, but only after checking to see
-        
-        if anything special need to be done (this is
-        what self._command_parser does)."""
-        state = self.factory.state
-        if any((not line,
-               self._command_parser(line),
-               state == "NOT CONNECTED")): return
-        
-        if state == "GET NAME":
-            self.name = line
-            #self.name_enc = self.encryptor.encrypt_ECB(self.name)
-            dict_to_send = Pk.makeDict(name=self.name, metadata={'newname': None})
-        elif state == "CONNECTED":
-            #iv, message = self.encryptor.encrypt(line)
-            dict_to_send = Pk.makeDict(name=self.name, msg=line)
-        line = Pk.packUp(dict_to_send)
-        self.write(line)
-    
     def write(self, line):
         if isinstance(line, str):
             line = line.encode()
         if not line.endswith(b'\r\n'):
             line += b'\r\n'
         self.transport.write(line)
-
-    def _command_parser(self, line):
-        """Checks if there are any commands in line.
-        
-        If there are, then they are dealt with. Note that
-        GUI specific commands should have been dealt with
-        already.
-        
-        If this function return True, then that means that
-        the calling function should not print line.
-        If it returns False, then the calling function
-        *should* print line to the screen (ie insert it
-        into the entrybox)."""
-        if not line.startswith('/'): return
-        line = line[1:].lower()
-        if line == 'warning':
-            self.line_received(Messages.warning_message)
-            return True
-        elif line == 'ping':
-            if self.factory.state == "CONNECTED":
-                self.write(Messages.ping_message)
-                self.ping_start = time.clock()
-                return True
-        elif any((line == 'list', line == 'listusers')):
-            if self.factory.state == "CONNECTED":
-                self.getUsers()
-                return True
-        return False
-
-    def getUsers(self):
-        """Asks the server for a list of people in the chatroom"""
-        self.write(Messages.getusers_message)
 
 
 class Factory(object):
