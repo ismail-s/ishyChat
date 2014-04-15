5ac34fcfc2dc3ee1c09be0604f0886d8e8db31e6
Ismail Suleman <astroman3D@gmail.com>
2014-04-15 14:04:31 +0100

    Create Py3 asyncio server based on BaseServer

---
+++ ishyChat/Server/Py3Server.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ishyChat.Utils.Packer as Packer
+from ishyChat.Utils.Packer import makeDictAndPack
+from ishyChat.Server.BaseServer import BaseServer
+
+import asyncio
+import ssl
+
+class PubProtocol(BaseServer, asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+        BaseServer.connection_made(self)
+
+    def data_received(self, line):
+        if isinstance(line, bytes):
+            line = line.decode()
+        # Note that line has '\r\n' at the end of it,
+        # but that doesn't seem to cause any problems
+        # with json decoding
+        #while not line.endswith('}'):
+            #line = line[:-1]
+        BaseServer.data_received(self, line)
+
+    def write(self, line):
+        if isinstance(line, str):
+            line = line.encode()
+        if not line.endswith(b'\r\n'):
+            line += b'\r\n'
+        self.transport.write(line)
+
+class PubFactory(object):
+    def __init__(self):
+        #this is a dict of nameTheClientChose: ClientClass
+        self.clients = {}
+
+    def run_reactor(self, port):
+        sslcontext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        sslcontext.load_cert_chain('ishyChat/Server/keys/cert.pem', keyfile = 'ishyChat/Server/keys/server.pem')
+        
+        # This code was copied from python3.4 docs, and modified a bit.
+        loop = asyncio.get_event_loop()
+        coro = loop.create_server(lambda : PubProtocol(self.clients),
+                                host = '',
+                                port = port,
+                                ssl = sslcontext)
+        server = loop.run_until_complete(coro)
+        print('serving on {}'.format(server.sockets[0].getsockname()))
+        
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            print("exit")
+        finally:
+            server.close()
+            loop.close()
