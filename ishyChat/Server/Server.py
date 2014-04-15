57ac7b3ffd4ff044e759a1b977cc1f9124892685
Ismail Suleman <astroman3D@gmail.com>
2014-04-15 14:03:30 +0100

    Remove redundant code/comments

--- ishyChat/Server/Server.py
+++ ishyChat/Server/Server.py
@@ -3,7 +3,6 @@
 
 from twisted.internet import reactor, protocol, ssl
 from twisted.protocols import basic
-import sys
 import ishyChat.Utils.Packer as Packer
 from ishyChat.Utils.Packer import makeDictAndPack
 from ishyChat.Server.BaseServer import BaseServer
@@ -20,7 +19,7 @@ class PubFactory(protocol.Factory):
     def __init__(self):
         #this is a dict of nameTheClientChose: ClientClass
         self.clients = {}
-        print "Server up and running."
+
 
     def buildProtocol(self, addr):
         #self.clients is passed so each client instance can send messages to the others.
@@ -32,6 +31,3 @@ def main(port):
     reactor.run()
 
 
-if __name__ == '__main__':
-    print """This is no longer intended to be run directly!
-Instead, run main.py in the highest directory (2 directories up)."""
