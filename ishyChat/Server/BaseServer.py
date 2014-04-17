bc2f4be2b42310991ec2a4054a1415cea6ef7fef
Ismail Suleman <astroman3D@gmail.com>
2014-04-17 19:11:53 +0100

    Make long function calls run over more lines

--- ishyChat/Server/BaseServer.py
+++ ishyChat/Server/BaseServer.py
@@ -17,7 +17,8 @@ class BaseServer(object):
 
     def connection_made(self):
         msg_to_send = "Welcome to ishyChat!\n{} other people present.\nWhat's your name?".format(len(self.clients))
-        self.write(makeDictAndPack(msg = msg_to_send, name = 'server', metadata = {'getname': None}))
+        self.write(makeDictAndPack(msg = msg_to_send, name = 'server',
+                                    metadata = {'getname': None}))
 
     def connectionLost(self, reason):
         return self.connection_lost(reason)
@@ -26,10 +27,14 @@ class BaseServer(object):
         if self.name in self.clients.keys():
             del self.clients[self.name]
         people = 'person' if (len(self.clients)-1) == 1 else 'people'
-        line = "{} has left. {} other {} still here.".format(self.name, len(self.clients)-1, people)
+        line = "{} has left. {} other {} still here.".format(self.name,
+                                                            len(self.clients)-1,
+                                                            people)
         for name, client in self.clients.items():
-            client.write(makeDictAndPack(msg = line, name = 'server', metadata = {'lostclient': None}))
-        to_print = ' '.join((self.name, "has left.", str(len(self.clients)), "clients connected."))
+            client.write(makeDictAndPack(msg = line, name = 'server',
+                                        metadata = {'lostclient': None}))
+        to_print = ' '.join((self.name, "has left.", str(len(self.clients)),
+                            "clients connected."))
         print(to_print)
 
     def lineReceived(self, line):
@@ -37,7 +42,8 @@ class BaseServer(object):
 
     def data_received(self, line):
         if 'ping' in (Packer.packDown(line))['metadata']:
-            self.write(makeDictAndPack(name = 'server', metadata = {'pong': None}))
+            self.write(makeDictAndPack(name = 'server',
+                                    metadata = {'pong': None}))
             return
         if self.state == Const_STATE_GETNAME:
             self.handle_GETNAME((Packer.packDown(line))['name'])
@@ -52,16 +58,19 @@ class BaseServer(object):
             self.write(makeDictAndPack(name = 'server', msg = message))
             return
         message = "Hiya {}!".format(name)
-        self.write(makeDictAndPack(name = 'server', metadata = {'gotname': None}, msg = message))
+        self.write(makeDictAndPack(name = 'server',
+                                    metadata = {'gotname': None},
+                                    msg = message))
         self.name, self.clients[name], self.state = name, self, Const.STATE_CHAT
 
         to_print = ' '.join((name, "has been added.",
         str(len(self.clients)),
         "client{} connected.".format('' if len(self.clients)  == 1 else 's')))
-        
+
         print(to_print)
         message = "{} has joined the chat.".format(name)
-        string = makeDictAndPack(name = 'server', metadata = ['newclient'], msg = message)
+        string = makeDictAndPack(name = 'server', metadata = ['newclient'],
+                                msg = message)
         for names, client in self.clients.items():
             if names != self.name:
                 client.write(string)
@@ -70,7 +79,8 @@ class BaseServer(object):
         # Maybe these next 2 lines should be moved into data_received
         metadata = Packer.packDown(line)['metadata']
         if 'getusers' in metadata:
-            self.write(makeDictAndPack(name = 'server', metadata = {'gotusers': list(self.clients.keys())}))
+            self.write(makeDictAndPack(name = 'server',
+                            metadata = {'gotusers': list(self.clients.keys())}))
             return
         # Need to work on this command so that everyone is alerted when someone
         # changes their name
