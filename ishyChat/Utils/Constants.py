cdb295a8dd25d2bdd6d5a5c8a160d0fead430620
Ismail <ismail-s.no-reply@github.com>
2014-05-10 17:46:30 +0100

    Add more constants for lex.py file (next commit)

--- ishyChat/Utils/Constants.py
+++ ishyChat/Utils/Constants.py
@@ -5,3 +5,13 @@ STATE_GETNAME = "GETNAME"
 STATE_CONNECTED = "CONNECTED"
 STATE_NOT_CONNECTED = "NOT CONNECTED"
 STATE_CHAT = "CHAT"
+
+#####################
+# Some constants used by lex.py and the views.
+# These constants are used to label different parts of messages,
+# so they can be dealt with spearately.
+MENTION = 'MENTION'
+HYPERLINK = 'HYPERLINK'
+WHITESPACE = 'WHITESPACE'
+NORMAL = 'NORMAL'
+#####################
